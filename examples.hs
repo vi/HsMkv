1d9b1281b391c53eb3861e93c72a173ce17cebca
Vitaly _Vi Shukela <vi0oss@gmail.com>
2014-11-14 21:06:00 +0300

    Add one more example: setpts
    (because of I need it now and FFmpeg can't do it)

--- examples.hs
+++ examples.hs
@@ -4,6 +4,7 @@ import qualified ExampleUdpRecv
 import qualified ExampleUdpSend
 import qualified ExampleTransmux
 import qualified ExamplePrint
+import qualified ExampleSetpts
 
 import Text.Printf
 import System.Environment (getArgs)
@@ -12,11 +13,12 @@ main = do
     args <- getArgs
     if length args < 1
         then do
-            printf "Usage: examples {udp_send|udp_recv|transmux|print}\n"
+            printf "Usage: examples {udp_send|udp_recv|transmux|print|setpts}\n"
             printf "   udp_send - read specified track from matroska file and send it as UDP datagrams\n"
             printf "   udp_recv - receive UDP messages and write them to matroska file\n"
             printf "   print    - read matroska file and print some information from it\n"
             printf "   transmux - read matroska file and write it\n"
+            printf "   setpts   - like transmux, but also fiddle with timecodes\n"
         else let
             (head1:tail1) = args
             in
@@ -25,3 +27,4 @@ main = do
                 "udp_recv" -> ExampleUdpRecv.mainUdpRecv tail1
                 "print" -> ExamplePrint.main
                 "transmux" -> ExampleTransmux.main
+                "setpts" -> ExampleSetpts.mainSetpts tail1
